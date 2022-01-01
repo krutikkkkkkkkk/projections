d3f916aba6bf0e5736d89ae3e6fb1a642e4f067c
reboot13-git <rautkrutik013@gmail.com>
2022-01-01 13:40:08 +0530

    Updated site

---
+++ js/gsap.js
@@ -0,0 +1,6 @@
+gsap.from(".about-head", {x:-100,opacity:0, duration: 1,ease:Power2.easeOut});
+gsap.from(".about-para", {y:-100,opacity:0, duration: 1,delay:1,ease:Power2.easeOut});
+gsap.from(".iframe-div", {x:100,opacity:0, duration: 1,delay:2,ease:Power2.easeOut});
+gsap.from(".stats-div",{x:-100,opacity:0,duration:2,delay:3,stagger:0.5,ease:Power2.easeOut})
+
+
